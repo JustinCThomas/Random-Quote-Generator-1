606f2221a411e59c60a7d9fc93628bbf6e82ef99
Justin Thomas <ThomasCJustin@gmail.com>
2018-02-01 17:55:22 -0500

    Initial Commit

---
+++ quote.js
@@ -0,0 +1,26 @@
+let quoteButton = document.getElementById("quote-button");
+let quoteContainer = document.getElementById("quote-container");
+let quote = document.getElementById("quoted");
+let author = document.getElementById("author");
+
+
+quoteButton.addEventListener("click", () => {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://talaikis.com/api/quotes/random/')
+
+
+  xhr.onload = () => {
+    let data = JSON.parse(xhr.responseText);
+
+    let quote = data.quote;
+    let author = data.author;
+    let category = data.cat;
+
+    window.quote.innerText = decodeURIComponent(quote);
+    window.author.innerText = `- ${author}`;
+  }
+
+
+  xhr.send();
+});
